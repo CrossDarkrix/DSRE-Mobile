0a7ff1415d0883fcd1f6c8b6fffb06b90b3f4677
CrossDarkrix <crosseddarkrix@gmail.com>
2026-05-23 09:42:39 +0900

    update code.

--- build-DSRE_mobile/build_DSRE-PY.py
+++
@@ -1,4 +0,0 @@
-import subprocess, sys
-subprocess.run("{} -m buildozer android init".format(sys.executable), shell=True)
-input("please write buildozer.spec, press enter to start build android apk")
-subprocess.run("{} -m buildozer android debug".format(sys.executable), shell=True)
